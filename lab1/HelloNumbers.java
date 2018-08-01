c5556d866500af20fe728aa068b151cff5f2e466
zheyu-wu <zheyu.wu@yahoo.com>
2018-08-01 10:19:39 -0700

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,13 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 10) {
+            int sum = 0;
+            for (int i = 0; i <= x; i++){
+                sum += i;
+            }
+            System.out.print(sum + " ");
+            x = x + 1;
+        }
+    }
+}
